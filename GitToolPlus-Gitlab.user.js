3fff3da822d489ad92270217c14d306ffeb62730
yxwmok <youxiangwomeng@sina.com>
2022-06-10 16:25:23 +0800

    Add files via upload

---
+++ GitToolPlus-Gitlab.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @icon         http://pic04.babytreeimg.com/img/common/136x136.png
+// @name         GitToolPlus-Gitlab
+// @namespace    http://www.babytree.com/
+// @version      4.10.3
+// @updateURL    https://alljc.cc/static/js/GitToolPlus-Gitlab.user.js
+// @downloadURL  https://alljc.cc/static/js/GitToolPlus-Gitlab.user.js
+// @description  Plus gitool!
+// @author       caoxinyu
+// @run-at       document-body
+// @include      http://gitool.plt.babytree-inc.com/*
+// @grant        GM_info
+// @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
+// @grant        GM_notification
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @connect      gitlab.babytree-inc.com
+// @connect      oapi.dingtalk.com
+// @connect      alljc.cc
+// @require      https://alljc.cc/static/js/GitoolPlus-Gitlab.min.js?39
+// ==/UserScript==
